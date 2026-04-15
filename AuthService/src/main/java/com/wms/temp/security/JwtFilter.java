7c427008ee21acaa4a67a2179783cdbec141445d
padolevrushabh <padolevrushabh26@gmail.com>
2026-04-15 17:01:43 +0530

    added Jwt security
    # Conflicts:
    #	AuthService/src/main/java/com/wms/temp/config/SecurityConfig.java
    #	AuthService/src/main/java/com/wms/temp/security/JwtFilter.java

--- AuthService/src/main/java/com/wms/temp/security/JwtFilter.java
+++ AuthService/src/main/java/com/wms/temp/security/JwtFilter.java
@@ -24,7 +24,7 @@ public class JwtFilter extends OncePerRequestFilter {
     
     public JwtFilter(JwtUtil jwtUtil) {
     	this.jwtUtil=jwtUtil;
-    	System.out.println("kk");
+
     }
 
     @Override
