8e089a93273ff730fc5c2617f160a1165438c7b4
padolevrushabh <padolevrushabh26@gmail.com>
2026-04-16 12:41:07 +0530

    product service initially added

---
+++ product-service/src/test/java/com/wms/temp/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wms.temp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
